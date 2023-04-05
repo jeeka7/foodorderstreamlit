2e41e6e4417d0715b38f4566d985816e3eda00f2
Dean Hunter <jeeka7@gmail.com>
2023-04-05 21:00:34 +0530

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -1,38 +1,68 @@
 import streamlit as st
 
-# Define the menu items and their prices
+# Define the menu items
 menu_items = {
-    "Burger": 10,
-    "Pizza": 15,
-    "Fries": 5,
-    "Salad": 8,
+    "Burger": 10.99,
+    "Pizza": 15.99,
+    "Fries": 5.99,
+    "Drink": 2.99
 }
 
-# Define the initial order
-order = []
+# Define the payment options
+payment_options = {
+    "Credit Card": "credit_card",
+    "Debit Card": "debit_card",
+    "PayPal": "paypal",
+    "Google Pay": "google_pay"
+}
 
-# Define the sidebar for adding items to the order
-st.sidebar.title("Menu")
-for item, price in menu_items.items():
-    quantity = st.sidebar.number_input(f"{item} (${price})", value=0)
-    if quantity > 0:
-        order.append({"item": item, "price": price, "quantity": quantity})
+# Define the order form
+def order_form():
+    order = {}
+    for item, price in menu_items.items():
+        qty = st.number_input(f"How many {item}s would you like?", min_value=0, max_value=10, step=1)
+        if qty > 0:
+            order[item] = {"quantity": qty, "price": price}
+    return order
 
-# Define the main section for displaying the order
-st.title("Your Order")
-if len(order) == 0:
-    st.write("No items in your order")
-else:
-    total = sum(item["price"] * item["quantity"] for item in order)
-    st.write(f"Total: ${total}")
-    st.write("Items:")
-    for item in order:
-        st.write(f"- {item['quantity']} x {item['item']} (${item['price']}) = ${item['quantity'] * item['price']}")
-def place_order(order):
-    # Here you would add code to process the order, e.g. sending it to a restaurant or delivery service.
-    # For this example, we'll just print the order to the console.
-    print("Order placed:")
-    for item in order:
-        print(f"- {item['quantity']} x {item['item']} (${item['price']}) = ${item['quantity'] * item['price']}")
-    total = sum(item["price"] * item["quantity"] for item in order)
-    print(f"Total: ${total}")
+# Define the payment form
+def payment_form():
+    payment = {}
+    selected_option = st.selectbox("Select a payment option", list(payment_options.keys()))
+    payment["option"] = payment_options[selected_option]
+    if payment["option"] == "credit_card" or payment["option"] == "debit_card":
+        payment["card_number"] = st.text_input("Enter your card number")
+        payment["expiry_date"] = st.date_input("Enter the expiry date")
+        payment["cvv"] = st.text_input("Enter the CVV")
+    elif payment["option"] == "paypal":
+        payment["email"] = st.text_input("Enter your PayPal email address")
+        payment["password"] = st.text_input("Enter your PayPal password", type="password")
+    elif payment["option"] == "google_pay":
+        payment["phone_number"] = st.text_input("Enter your phone number")
+        payment["pin"] = st.text_input("Enter your PIN", type="password")
+    return payment
+
+# Define the main app
+def main():
+    st.title("Food Ordering App")
+    order = order_form()
+    st.write("Your order:")
+    for item, details in order.items():
+        st.write(f"{item} x {details['quantity']} - ${details['price'] * details['quantity']:.2f}")
+    total_price = sum([details['price'] * details['quantity'] for details in order.values()])
+    st.write(f"Total: ${total_price:.2f}")
+    payment = payment_form()
+    st.write("Payment details:")
+    st.write(f"Option: {list(payment_options.keys())[list(payment_options.values()).index(payment['option'])]}")
+    if payment["option"] == "credit_card" or payment["option"] == "debit_card":
+        st.write(f"Card Number: {payment['card_number']}")
+        st.write(f"Expiry Date: {payment['expiry_date']}")
+        st.write(f"CVV: {payment['cvv']}")
+    elif payment["option"] == "paypal":
+        st.write(f"PayPal Email: {payment['email']}")
+        st.write(f"PayPal Password: {len(payment['password']) * '*'}")
+    elif payment["option"] == "google_pay":
+        st.write(f"Phone Number: {payment['phone_number']}")
+        st.write(f"PIN: {len(payment['pin']) * '*'}")
+
+if __name__ == "__
