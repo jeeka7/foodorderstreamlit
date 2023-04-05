76a08c05d548fe4a89eccc2e599c7ec6cdbcfdfc
Dean Hunter <jeeka7@gmail.com>
2023-04-05 22:34:32 +0530

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -1,21 +1,25 @@
 import streamlit as st
 
-# Define the food items and their prices
-menu = {"Pizza": 10, "Burger": 5, "Hot Dog": 4, "Fries": 3}
+# Define the food items available for order
+food_items = {
+    "Pizza": 10.99,
+    "Burger": 7.99,
+    "Fries": 3.99,
+    "Chicken Wings": 8.99
+}
 
-# Create a sidebar menu with the food items
-st.sidebar.title("Menu")
-food_choice = st.sidebar.selectbox("Select a food item", list(menu.keys()))
+# Create a sidebar menu of food items
+st.sidebar.header("Menu")
+selected_item = st.sidebar.selectbox("Select an item", list(food_items.keys()))
 
-# Get the quantity of the selected food item
-quantity = st.sidebar.number_input("Quantity", min_value=1, max_value=10, value=1)
+# Create a quantity selector
+quantity = st.sidebar.number_input("Select quantity", min_value=1, max_value=10, value=1)
 
-# Calculate the total price of the order
-total_price = menu[food_choice] * quantity
+# Calculate the total price based on the selected item and quantity
+total_price = food_items[selected_item] * quantity
 
-# Show the total price to the user
-st.write(f"Total price: ${total_price}")
-
-# Allow the user to place the order
-if st.button("Place Order"):
-    st.write("Your order has been placed!")
+# Display the order details
+st.write("## Order Details")
+st.write(f"Item: {selected_item}")
+st.write(f"Quantity: {quantity}")
+st.write(f"Total price: ${total_price:.2f}")
