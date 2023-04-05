d28345e551f98d0bc4cba531f9e527188f9c7116
Dean Hunter <jeeka7@gmail.com>
2023-04-05 21:23:31 +0530

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -1,53 +1,20 @@
 import streamlit as st
 
-# Define menu items and their prices
-menu = {
-    "Burger": 10.0,
-    "Pizza": 20.0,
-    "Sandwich": 5.0,
-    "Fries": 3.0
-}
+# Set up the sidebar menu
+st.sidebar.title("Menu")
+menu_items = ["Burger", "Pizza", "Salad"]
+selected_item = st.sidebar.selectbox("Select a menu item", menu_items)
 
-# Define a function to calculate the total cost of the order
-def calculate_total(order):
-    total = 0.0
-    for item, quantity in order.items():
-        total += menu[item] * quantity
-    return total
+# Set up the food order form
+st.title("Food Order Form")
+name = st.text_input("Name")
+address = st.text_input("Address")
+phone = st.text_input("Phone")
+quantity = st.number_input("Quantity", min_value=1, max_value=10, step=1)
+delivery = st.radio("Delivery method", ("Delivery", "Pickup"))
 
-# Define the main function
-def main():
-    st.title("Food Ordering and Payment System")
-
-    # Display the menu
-    st.write("## Menu")
-    for item, price in menu.items():
-        st.write(f"{item}: ${price:.2f}")
-
-    # Get the user's order
-    st.write("## Order")
-    order = {}
-    for item in menu.keys():
-        quantity = st.number_input(f"How many {item}s do you want?", min_value=0, value=0)
-        if quantity > 0:
-            order[item] = quantity
-
-    # Calculate the total cost
-    total = calculate_total(order)
-
-    # Display the total cost
-    st.write("## Total")
-    st.write(f"Total cost: ${total:.2f}")
-
-    # Get the user's UPI ID
-    st.write("## Payment")
-    upi_id = st.text_input("Enter your UPI ID")
-
-    # Allow the user to confirm the order and pay
-    if st.button("Confirm order and pay"):
-        # TODO: Implement the UPI payment functionality here
-        st.write(f"Your order has been confirmed and paid for. Thank you for ordering!")
-
-# Run the main function
-if __name__ == "__main__":
-    main()
+# Submit the order
+if st.button("Place Order"):
+    st.write(f"Thank you for your order, {name}!")
+    st.write(f"You ordered {quantity} {selected_item}(s).")
+    st.write(f"Your {delivery.lower()} address is {address}. We will call you at {phone} to confirm your order.")
