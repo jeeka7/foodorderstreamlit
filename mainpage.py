13794a6df10f9a7014289f8309e5bb3f54b42be5
Dean Hunter <jeeka7@gmail.com>
2023-04-05 20:38:53 +0530

    Create mainpage.py

---
+++ mainpage.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+# Define food items and prices
+menu = {
+    "Burger": 10,
+    "Pizza": 12,
+    "Salad": 8,
+    "Fries": 4
+}
+
+# Initialize cart
+cart = {}
+
+# Display menu and accept user input
+st.title("Food Ordering App")
+st.write("Welcome to our menu!")
+for item, price in menu.items():
+    st.write(f"{item}: ${price}")
+    quantity = st.number_input(f"Enter quantity for {item}", min_value=0, max_value=10)
+    if quantity > 0:
+        cart[item] = quantity
+
+# Display cart and total
+st.write("Your Cart:")
+for item, quantity in cart.items():
+    st.write(f"{item} x {quantity}")
+total = sum([menu[item]*quantity for item, quantity in cart.items()])
+st.write(f"Total: ${total}")
+
+# Place order
+if st.button("Place Order"):
+    st.write("Thank you for your order!")
+    cart.clear()
